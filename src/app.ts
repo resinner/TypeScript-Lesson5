7ff43118edc211bca8b5a99556ffac00fe412f90
resinner <revengingsinner@gmail.com>
2023-08-16 14:39:19 +0300

    Lesson 5 done

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,63 @@
-const some = 'string';
+function getPromise(): Promise<Array<string | number>> {
+  return new Promise((resolve) => {
+    resolve(["Text", 50]);
+  });
+}
+
+getPromise().then((data) => {
+  console.log(data);
+});
+
+
+//================================================================
+
+
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
+
+function compare(top: Pick<AllType, 'name' | 'color'>, bottom: Pick<AllType, 'position' | 'weight'>): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
+
+
+//================================================================
+
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const merged = merge({ name: 'John' }, { age: 34 });
+
+console.log(merged.name);
+
+
+//================================================================
+
+
+class Component<T> {
+  constructor(public props: T) {}
+}
+
+interface IT {
+    title: string;
+}
+
+class Page extends Component<IT> {
+  pageInfo() {
+    console.log(this.props.title);
+  }
+}
+
+const ITitle = new Page({ title: 'Javascript' });
+
+ITitle.pageInfo();
